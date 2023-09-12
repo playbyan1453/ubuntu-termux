cfc028a76f336ffbd3c4ba531274c784846bb51e
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2023-09-12 13:00:40 +0700

    Add files via upload
    Ubuntu 23.04 fixed some typos and removed browser install

---
+++ de-xfce.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Get the necessary components
+apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo tzdata -y
+apt-get install xfce4 xfce4-goodies xfce4-terminal parole -y
+apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get --fix-broken install
+apt-get clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+startxfce4" > ~/.vnc/xstartup
+
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+dbus-launch startxfce4" > /usr/local/bin/vncstart
+   echo "vncserver -geometry 800x600 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+   chmod +x ~/.vnc/xstartup
+   chmod +x /usr/local/bin/*
+   clear
+
+rm -rf /etc/apt/sources.list
+mv sources.list /etc/apt/
+rm .vnc/passwd
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo "Start VNC Server, run vnc-start"
+   echo "Stop VNC Server, run vnc-stop"
+   echo ""
+rm de-xfce.sh
