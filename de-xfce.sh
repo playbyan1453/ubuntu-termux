934e4c786e860162d6b0854b532eaa5efd45888e
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2023-09-15 12:40:50 +0700

    Update de-xfce.sh
    Fix crash first boot, reduced bloatware installation.

--- de-xfce.sh
+++ de-xfce.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #Get the necessary components
 apt-get update
-apt-get install udisks2 -y
-echo " " > /var/lib/dpkg/info/udisks2.postinst
-apt-mark hold udisks2
-apt-get install sudo tzdata -y
-apt-get install xfce4 xfce4-goodies xfce4-terminal parole -y
+apt-get install xfce4 xfce4-terminal xfce4-whiskermenu-plugin -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
 apt-get clean
