fddc0ee5ca20b1d3b2f7a1bc003f7c6c83ea5c75
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2023-09-17 02:56:35 +0700

    Separate Firefox installation
    Separated Firefox instalaltion, optional to install. xfce.sh removed whiskermenu plugin.

--- desktopenv/lxqt.sh
+++ desktopenv/lxqt.sh
@@ -24,32 +24,6 @@ dbus-launch startlxqt" > /usr/local/bin/vncstart
    chmod +x /usr/local/bin/*
    clear
    echo ""
-   echo "Installing Browser..."
-   echo ""
-#Browser Fix
-cp /etc/apt/sources.list ~/
-wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
-echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt update
-apt install firefox-esr -y
-vnc-start
-sleep 5
-DISPLAY=:1 firefox &
-sleep 10
-pkill -f firefox
-vnc-stop
-sleep 2
-
-wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/playbyan1453/ubuntu-termux/main/user.js
-
-rm -rf /etc/apt/sources.list
-mv sources.list /etc/apt/
-rm .vnc/passwd
-   clear
-   echo ""
    echo "VNC Server address will run at 127.0.0.1:5901"
    echo "Start VNC Server, run vnc-start"
    echo "Stop VNC Server, run vnc-stop"
--- desktopenv/xfce.sh
+++ desktopenv/xfce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Get the necessary components
 apt-get update
-apt-get install xfce4 xfce4-terminal xfce4-whiskermenu-plugin -y
+apt-get install xfce4 xfce4-terminal -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
 apt-get clean
@@ -22,32 +22,6 @@ dbus-launch startxfce4" > /usr/local/bin/vncstart
    echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
    chmod +x ~/.vnc/xstartup
    chmod +x /usr/local/bin/*
-   clear
-   echo ""
-   echo "Installing Browser..."
-   echo ""
-#Browser Fix
-cp /etc/apt/sources.list ~/
-wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
-echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt update
-apt install firefox-esr -y
-vnc-start
-sleep 5
-DISPLAY=:1 firefox &
-sleep 10
-pkill -f firefox
-vnc-stop
-sleep 2
-
-wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/playbyan1453/ubuntu-termux/main/user.js
-
-rm -rf /etc/apt/sources.list
-mv sources.list /etc/apt/
-rm .vnc/passwd
    clear
    echo ""
    echo "VNC Server address will run at 127.0.0.1:5901"
