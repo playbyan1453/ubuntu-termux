db3459a377b8ab3b333f3d62f50d7637379cca1b
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2024-09-10 20:56:19 +0700

    Update xfce.sh

--- desktopenv/xfce.sh
+++ desktopenv/xfce.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #Get the necessary components
 apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo tzdata -y
 apt-get install xfce4 xfce4-terminal -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
