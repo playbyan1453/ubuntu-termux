54f4aac6e2383bce2d384761ba9afdec2cdbf6c7
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2024-09-11 18:54:41 +0700

    Update lxqt.sh

--- desktopenv/lxqt.sh
+++ desktopenv/lxqt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #Get the necessary components
 apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo tzdata -y
 apt-get install lxqt qterminal -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
@@ -18,14 +22,15 @@ export DISPLAY=:1
 export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
 dbus-launch startlxqt" > /usr/local/bin/vncstart
-   echo "vncserver -geometry 800x600 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -geometry 1280x720 -name remote-desktop :1" > /usr/local/bin/vnc-start
    echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
    chmod +x ~/.vnc/xstartup
    chmod +x /usr/local/bin/*
+
    clear
    echo ""
    echo "VNC Server address will run at 127.0.0.1:5901"
    echo "Start VNC Server, run vnc-start"
-   echo "Stop VNC Server, run vnc-stop"
+   echo "Stop  VNC Server, run vnc-stop"
    echo ""
 rm lxqt.sh
