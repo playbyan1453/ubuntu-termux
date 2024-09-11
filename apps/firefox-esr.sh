b1e414efbeaa35075940f152a7ce4086b0a6fb2d
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2024-09-11 20:00:18 +0700

    Minor changes

--- apps/firefox-esr.sh
+++ apps/firefox-esr.sh
@@ -8,8 +8,9 @@ echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt update
-apt install firefox-esr -y
+
+apt update ; apt install firefox-esr -y
+
 vnc-start
 sleep 5
 DISPLAY=:1 firefox &
